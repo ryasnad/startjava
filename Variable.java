543e1d503b503ef72e47187c5e2934a44bc48e4e
ryasnad <dariaryasnyanskaya@gmail.com>
2020-08-22 17:35:56 +0300

    first commit

---
+++ Variable.java
@@ -0,0 +1,22 @@
+public class Variable{
+	public static void main(String[] args){
+	// TODO: Объявите в методе переменные всех существующих в Java примитивных типов данных.
+	byte  operating_system = 64; 
+	short  system_memory = 8; 
+	int grafic_memory = 2481; 
+	long dcdkjf = 9373662;
+	float bios = 65f; 
+	double biod = 7.7622d; 
+	char dkdcndncjd = 'd'; 
+	boolean r = true; 
+
+	System.out.println(operating_system); 
+	System.out.println(system_memory); 
+	System.out.println(grafic_memory); 
+	System.out.println(dcdkjf); 
+	System.out.println(bios); 
+	System.out.println(biod); 
+	System.out.println(dkdcndncjd); 
+	System.out.println(r); 
+	}
+}
